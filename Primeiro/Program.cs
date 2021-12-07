f42f642329479fdbecbc6af6354697601cc6d6d9
Perkles <almeidamyrelalima@gmail.com>
2021-12-07 14:54:16 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Primeiro
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "hello";
+            var b = "world";
+            Console.WriteLine(a + b);
+        }
+    }
+}
